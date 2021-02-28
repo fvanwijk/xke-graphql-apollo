37189ba20c2bc53f1515c877a55bbbd7a3665828
Frank van Wijk <fvanwijk@xebia.com>
2021-03-01 00:09:08 +0100

    Setup basic static home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <div className="container">
+          <h1>XKE - GraphQL Apollo</h1>
+        </div>
       </header>
+      <main>
+        <div className="container">
+          <h2>My books</h2>
+          No books queried yet
+        </div>
+      </main>
     </div>
   );
 }
