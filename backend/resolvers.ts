1154d598a358ef88d6b92365cca3ba98fbd56c2a
Frank van Wijk <fvanwijk@xebia.com>
2021-02-28 13:24:22 +0100

    Add query to get book by id

--- backend/resolvers.ts
+++ backend/resolvers.ts
@@ -5,10 +5,12 @@ const authors = [
 
 const books = [
   {
+    id: 1,
     title: "Harry Potter and the Sorcerer's stone",
     author: authors[0],
   },
   {
+    id: 2,
     title: "Jurassic Park",
     author: authors[1],
   },
@@ -17,5 +19,6 @@ const books = [
 export const resolvers = {
   Query: {
     books: () => books,
+    book: (_: unknown, { id }: { id: number }) => books.find((book) => book.id === id),
   },
 };
